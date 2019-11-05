4639d0b65abc073d7f2a58b0cd5812db5e26ceae
gabrijela <gabrijela@q-alliance.com>
2019-11-05 17:42:34 +0100

    Navigation hover fix, navigation text wrap

--- app/js/main.js
+++ app/js/main.js
@@ -17,3 +17,14 @@ $(function () {
 
     });
 });
+
+// $("ul.dropdown-menu [data-toggle='dropdown']").on("mouseenter", function(e) {
+//     var submenu = $(this);
+//     $('.multi-level-dropdown .dropdown-submenu .dropdown-menu').removeClass('show');
+//     submenu.next('.dropdown-menu').addClass('show');
+//     e.stopPropagation();
+//   });
+// $('.multi-level-dropdown .dropdown').on("hidden.bs.dropdown", function() {
+//   // hide any open menus when parent closes
+//   $('.multi-level-dropdown .dropdown-menu.show').removeClass('show');
+// });
